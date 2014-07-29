c5005b30da51493626c450fe27ffa93a278ba7a9
Roman <myrage24@yandex.ru>
2014-07-29 05:21:52 +0400

    0.0.6  - characters templates + bug animation

---
+++ Mystie the Fox/MFBug.h
@@ -0,0 +1,13 @@
+//
+//  MFBug.h
+//  Mystie the Fox
+//
+//  Created by Roman on 28.07.14.
+//  Copyright (c) 2014 Roman. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MFBug : SKSpriteNode
+
+@end
